f716c0968e9aca8700303cf2ad8c07ec72efcd9b
sunnythelucky <sunnyparkk@gmail.com>
2022-06-10 16:23:48 -0400

    refactor detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,7 +1,7 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 import meetupImage from "../../public/Munich.jpeg";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
 	return (
 		<MeetupDetail
 			title="Our 1st Meetup at Munich"
@@ -10,5 +10,42 @@ const MeetupDetails = () => {
 		/>
 	);
 };
+
+export function getStaticPaths() {
+	return {
+		// set fallback as false means that you made all the supported path down below
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: "m1",
+				},
+			},
+			{
+				params: {
+					meetupId: "m2",
+				},
+			},
+		],
+	};
+}
+
+export async function getStaticProps(context) {
+	//fetch data for a single meetup
+	const meetupId = context.params.meetupId;
+	console.log(meetupId);
+
+	return {
+		props: {
+			meetupData: {
+				title: "Our 1st Meetup at Munich",
+				address: "28 Rotkreuzplatz, Munich, 29301, Bavaria, Germany",
+				description: "Just come with your Bier and Bretzel",
+				image: await meetupImage,
+				id: meetupId,
+			},
+		},
+	};
+}
 
 export default MeetupDetails;
