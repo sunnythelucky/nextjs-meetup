bbe00655b5811bcdf83c5f7b977515cfb6d30723
sunnythelucky <sunnyparkk@gmail.com>
2022-06-09 22:47:51 -0400

    add components

---
+++ components/meetups/MeetupDetail.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import Image from "next/image";
+import meetupImage from "../../public/Munich.jpeg";
+import classes from "./MeetupDetail.module.css";
+
+const MeetupDetail = (props) => {
+	return (
+		<section className={classes.detail}>
+			<Image src={meetupImage} alt="FrauenKirche" />
+			<h1 className={classes.title}>{props.title}</h1>
+			<address>{props.address}</address>
+			<p>{props.description}</p>
+		</section>
+	);
+};
+
+export default MeetupDetail;
