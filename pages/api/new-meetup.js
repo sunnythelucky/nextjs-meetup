b82772b91d22bf83721a0712b77522ed908d642b
sunnythelucky <sunnyparkk@gmail.com>
2022-06-10 16:22:50 -0400

    add api

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,11 @@
+// /api/new-meetup
+// POST /api/new-meetup
+
+function handler(req, res) {
+	if ((req.method = "POST")) {
+		const data = req.body;
+		const { title, address, description, image } = data;
+	}
+}
+
+export default handler;
