b444c1a62661ac53a8b57f2725c19d296045a3b4
sunnythelucky <sunnyparkk@gmail.com>
2022-06-14 17:04:25 -0400

    add server

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -5,7 +5,7 @@ import classes from "./MeetupDetail.module.css";
 const MeetupDetail = (props) => {
 	return (
 		<section className={classes.detail}>
-			<Image src={meetupImage} alt="FrauenKirche" />
+			<Image src={meetupImage} alt={props.image.alt} layout="fill" />
 			<h1 className={classes.title}>{props.title}</h1>
 			<address>{props.address}</address>
 			<p>{props.description}</p>
--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,29 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
 	if ((req.method = "POST")) {
-		const data = req.body;
-		const { title, address, description, image } = data;
+		try {
+			const data = req.body;
+
+			const client = await MongoClient.connect(
+				"mongodb+srv://sunnyparkk:qlalfqjsgh123@cluster0.n13wv.mongodb.net/meetups?retryWrites=true&w=majority"
+			);
+			const db = client.db();
+
+			const meetupsCollection = db.collection("meetups");
+
+			const result = await meetupsCollection.insertOne(data);
+			console.log(result);
+			client.close();
+
+			res.status(201).json({ message: "Meetup created successfully" });
+		} catch (e) {
+			console.error(e);
+			res.status(500).json({ message: "Something went wrong, this is the error you got : " + e });
+		}
 	}
 }
 
